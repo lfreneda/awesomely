22c411b27c24bcb9af4ee7543248e4197f533c88
Luiz Freneda <lfreneda@gmail.com>
2013-05-25 17:51:39 -0300

    Enumerable extensions

---
+++ src/awesomely/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace awesomely.Extensions {
+
+    public static class EnumerableExtensions {
+        public static void Each<T>(this IEnumerable<T> source, Action<T> action) {
+            foreach (var item in source) action(item);
+        }
+    }
+}
---
+++ src/awesomely/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace awesomely.Extensions
+{
+    public static class StringExtensions {
+        public static string RemoveSpecialCharacters(this string str) {
+            var r = new Regex("(?:[^a-z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            return r.Replace(str, String.Empty);
+        }
+    }
+}
