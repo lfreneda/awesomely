7dd1f41e8020acce6fa1d0fd4fa5984a0d57fbb1
Luiz Freneda <lfreneda@gmail.com>
2013-05-26 20:44:00 -0300

    Adding ToFile Enumerable extension

--- src/awesomely/Extensions/EnumerableExtensions.cs
+++ src/awesomely/Extensions/EnumerableExtensions.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace awesomely.Extensions {
 
     public static class EnumerableExtensions {
-        public static void Each<T>(this IEnumerable<T> source, Action<T> action) {
-            foreach (var item in source) action(item);
+        public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action) {
+            foreach (var item in enumerable) action(item);
+        }
+
+        public static void ToFile<T>(this IEnumerable<T> enumerable, string filePath) {
+            using (var sw = new StreamWriter(filePath, true)) {
+                foreach (var item in enumerable) {
+                    sw.WriteLine(item);
+                } sw.Close();
+            }
         }
     }
 }
