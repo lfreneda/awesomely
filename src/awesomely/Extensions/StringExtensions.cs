4c94fdc5b7a66ef192223aecf6f373fe40b8e577
Luiz Freneda <lfreneda@gmail.com>
2013-05-27 09:58:53 -0300

    StringExtensions FormatWith \ ContainsIn

--- src/awesomely/Extensions/StringExtensions.cs
+++ src/awesomely/Extensions/StringExtensions.cs
@@ -8,5 +8,19 @@ namespace awesomely.Extensions
             var r = new Regex("(?:[^a-z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
             return r.Replace(str, String.Empty);
         }
+		
+        public static string FormatWith(this string format, params object[] args) {
+            if (format == null) throw new ArgumentNullException("format");
+            return string.Format(format, args);
+        }
+
+        public static string FormatWith(this string format, IFormatProvider provider, params object[] args) {
+            if (format == null) throw new ArgumentNullException("format");
+            return string.Format(provider, format, args);
+        }
+
+		public static bool ContainsIn(this string str, string @in) {
+            return @in.IndexOf(str, StringComparison.Ordinal) != -1;
+        }
     }
 }
