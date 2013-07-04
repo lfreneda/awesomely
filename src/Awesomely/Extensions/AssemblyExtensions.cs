92ea8e4dfb917e554d7f46fdfcba7af0598f6a84
Luiz Freneda <lfreneda@gmail.com>
2013-07-04 11:08:58 -0300

    AssemblyExtensions - creating instances from T

---
+++ src/Awesomely/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace awesomely.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<T> GetInstancesOf<T>(this Assembly assembly)
+        {
+            var types = assembly
+                .GetTypes()
+                .Where(x => ((typeof(T).IsAssignableFrom(x) && x.IsClass && !x.IsAbstract)));
+
+            return types.Select(t => (T)Activator.CreateInstance(t));
+        }
+    }
+}
--- src/awesomely/Extensions/EnumerableExtensions.cs
+++ src/awesomely/Extensions/EnumerableExtensions.cs
@@ -6,7 +6,6 @@ namespace awesomely.Extensions
 {
     public static class EnumerableExtensions
     {
-
         public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
         {
             foreach (var item in enumerable) action(item);
