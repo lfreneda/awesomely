72be60ad3adf9e07fa93395d99fc8cd8dccc0e61
Luiz Freneda <lfreneda@gmail.com>
2013-07-04 10:55:49 -0300

    Refactory Reflection helper

---
+++ src/Awesomely/Reflection/Reflector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Awesomely.Reflection
+{
+    public static class Reflector
+    {
+        public static IEnumerable<T> GetInstancesOf<T>()
+        {
+            var types = Assembly
+                  .GetExecutingAssembly()
+                  .GetTypes()
+                  .Where(x => ((typeof(T).IsAssignableFrom(x) && x.IsClass && !x.IsAbstract)));
+
+            return types.Select(t => (T)Activator.CreateInstance(t));
+        }
+    }
+}
