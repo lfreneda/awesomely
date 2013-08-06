931c4c9572076069e9eb3ecf23fc11af94c52ae3
Luiz Freneda <lfreneda@gmail.com>
2013-08-06 16:44:36 -0300

    Enumerable Extensions - Split method

--- src/awesomely/Extensions/EnumerableExtensions.cs
+++ src/awesomely/Extensions/EnumerableExtensions.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Awesomely.Extensions
 {
     public static class EnumerableExtensions
     {
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            return source.Where((x, i) => i % chunkSize == 0).Select((x, i) => source.Skip(i * chunkSize).Take(chunkSize));
+        }
+
         public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
         {
             foreach (var item in enumerable) action(item);
