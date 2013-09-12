36b8d8952aec162842839609625158a816a32903
Luiz Freneda <lfreneda@gmail.com>
2013-09-11 23:56:09 -0300

    Secure integration with Facebook

--- src/Awesomely/OAuth/Facebook/FacebookUriAuthentication.cs
+++ src/Awesomely/OAuth/Facebook/FacebookUriAuthentication.cs
@@ -2,23 +2,40 @@ namespace Awesomely.OAuth.Facebook
 {
     public class FacebookUriAuthentication : IUriAuthentication
     {
-        private readonly IConfiguration _configuration; 
-        public FacebookUriAuthentication(IConfiguration configuration) {
+        private readonly bool _secure;
+        private readonly IConfiguration _configuration;
+        public FacebookUriAuthentication(IConfiguration configuration)
+        {
             _configuration = configuration;
         }
 
-        public FacebookUriAuthentication() {
+        public FacebookUriAuthentication(bool secure)
+        {
+            _secure = secure;
             _configuration = new FacebookConfiguration();
         }
 
-        public string GetUriInitializeAuthentication() {
-            return string.Format("https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&scope=email", 
-                _configuration.ApplicationId, _configuration.RedirectUri);
+        public string GetUriInitializeAuthentication()
+        {
+            var redirectUrl = _configuration.RedirectUri;
+            if (_secure)
+            {
+                redirectUrl = redirectUrl.Replace("http://", "https://");
+            }
+
+            return string.Format("https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&scope=email",
+                _configuration.ApplicationId, redirectUrl);
         }
 
-        public string GetUriTokenRetrieve(string authorizationCode) {
+        public string GetUriTokenRetrieve(string authorizationCode)
+        {
+            var redirectUrl = _configuration.RedirectUri;
+            if (_secure)
+            {
+                redirectUrl = redirectUrl.Replace("http://", "https://");
+            }
             return string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}",
-                _configuration.ApplicationId, _configuration.RedirectUri, _configuration.ApplicationSecret, authorizationCode);
+                _configuration.ApplicationId, redirectUrl, _configuration.ApplicationSecret, authorizationCode);
         }
     }
 }
